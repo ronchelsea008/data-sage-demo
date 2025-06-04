0b23e17877757d06f50e7b51f7bd2a9a93934025
ronchelsea008 <ron.sg008@gmail.com>
2025-06-04 23:44:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"
 
 if uploaded_file is not None:
     try:
-        if uploaded_file.name.endswith(".csv"):
+    if uploaded_file.name.endswith(".csv"):
     preview = pd.read_csv(uploaded_file, header=None, nrows=10)
     threshold = preview.shape[1] * 0.5
     probable_header_row = preview.apply(lambda row: row.count(), axis=1).gt(threshold).idxmax()
